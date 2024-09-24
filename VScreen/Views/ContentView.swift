1293d555b3768bd09a24d24e17002af5a5f6ae47
eakaraman <eakaraman1@gmail.com>
2024-09-24 21:09:17 +0300

    added pause overlay

--- VScreen/Views/ContentView.swift
+++ VScreen/Views/ContentView.swift
@@ -14,131 +14,146 @@ struct ContentView: View {
     @StateObject var screenRecorder = ScreenRecorder()
     
     var body: some View {
-        screenRecorder.capturePreview
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .aspectRatio(screenRecorder.contentSize, contentMode: .fit)
-            .overlay {
-                if isUnauthorized {
-                    VStack() {
-                        Spacer()
-                        VStack {
-                            Text("No screen recording permission.")
-                                .font(.largeTitle)
-                                .padding(.top)
-                            Text("Open System Settings and go to Privacy & Security > Screen Recording to grant permission.")
-                                .font(.title2)
-                                .padding(.bottom)
+        ZStack {
+            screenRecorder.capturePreview
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .aspectRatio(screenRecorder.contentSize, contentMode: .fit)
+                .overlay {
+                    if isUnauthorized {
+                        VStack() {
+                            Spacer()
+                            VStack {
+                                Text("No screen recording permission.")
+                                    .font(.largeTitle)
+                                    .padding(.top)
+                                Text("Open System Settings and go to Privacy & Security > Screen Recording to grant permission.")
+                                    .font(.title2)
+                                    .padding(.bottom)
+                            }
+                            .frame(maxWidth: .infinity)
+                            .background(.red)
+                            
                         }
-                        .frame(maxWidth: .infinity)
-                        .background(.red)
-                        
                     }
                 }
-            }
-            .navigationTitle("VScreen")
-            .onAppear {
-                Task {
-                    if await screenRecorder.canRecord {
-                        await screenRecorder.start()
-                    } else {
-                        isUnauthorized = true
-                    }
-                }
-            }
-            .onWindow { window in
-                self.window = window
-                window?.level = isWindowFloating ? .floating : .normal
-                window?.alphaValue = opacity
-            }
-            .toolbar {
-                VStack {
-                    Picker("captureType", selection: $screenRecorder.captureType) {
-                        Text("Display")
-                            .tag(ScreenRecorder.CaptureType.display)
-                        Text("Window")
-                            .tag(ScreenRecorder.CaptureType.window)
-                    }.labelsHidden()
-                }
-                VStack {
-                    switch screenRecorder.captureType {
-                    case .display:
-                        Picker("display", selection: $screenRecorder.selectedDisplay) {
-                            ForEach(screenRecorder.availableDisplays, id: \.self) { display in
-                                Text(display.displayName)
-                                    .tag(SCDisplay?.some(display))
-                            }
-                        }.labelsHidden()
-                        
-                    case .window:
-                        Picker("window", selection: $screenRecorder.selectedWindow) {
-                            ForEach(screenRecorder.availableWindows, id: \.self) { window in
-                                Text(window.displayName)
-                                    .tag(SCWindow?.some(window))
-                            }
-                        }.labelsHidden()
-                    }
-                }
-                
-                Button(action: {
+                .navigationTitle("VScreen")
+                .onAppear {
                     Task {
                         if await screenRecorder.canRecord {
-                            if (screenRecorder.isRunning) {
-                                await screenRecorder.stop()
-                            }
-                            else {
-                                await screenRecorder.start()
-                            }
+                            await screenRecorder.start()
                         } else {
                             isUnauthorized = true
                         }
                     }
-                }) {
-                    Image(systemName: screenRecorder.isRunning ? "stop.circle" : "play.circle")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
                 }
-                .buttonStyle(PlainButtonStyle())
-                
-                Slider(value: $opacity, in: 0.3...1.0)
-                    .frame(width: 100)
-                    .onChange(of: opacity) {
-                        window?.alphaValue = opacity
+                .onWindow { window in
+                    self.window = window
+                    window?.level = isWindowFloating ? .floating : .normal
+                    window?.alphaValue = opacity
+                }
+                .toolbar {
+                    VStack {
+                        Picker("captureType", selection: $screenRecorder.captureType) {
+                            Text("Display")
+                                .tag(ScreenRecorder.CaptureType.display)
+                            Text("Window")
+                                .tag(ScreenRecorder.CaptureType.window)
+                        }.labelsHidden()
                     }
-                
-                Button(action: {
-                    screenRecorder.isAppExcluded.toggle()
-                }) {
-                    Image(systemName: screenRecorder.isAppExcluded ? "eye.slash" : "eye")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                }
-                .buttonStyle(PlainButtonStyle())
-                
-                Button(action: {
-                    isWindowFloating.toggle()
-                    if let window = window {
-                        window.level = isWindowFloating ? .floating : .normal
+                    VStack {
+                        switch screenRecorder.captureType {
+                        case .display:
+                            Picker("display", selection: $screenRecorder.selectedDisplay) {
+                                ForEach(screenRecorder.availableDisplays, id: \.self) { display in
+                                    Text(display.displayName)
+                                        .tag(SCDisplay?.some(display))
+                                }
+                            }.labelsHidden()
+                            
+                        case .window:
+                            Picker("window", selection: $screenRecorder.selectedWindow) {
+                                ForEach(screenRecorder.availableWindows, id: \.self) { window in
+                                    Text(window.displayName)
+                                        .tag(SCWindow?.some(window))
+                                }
+                            }.labelsHidden()
+                        }
                     }
-                }) {
-                    Image(systemName: isWindowFloating ? "rectangle.on.rectangle" : "rectangle")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
+                    
+                    Button(action: {
+                        Task {
+                            if await screenRecorder.canRecord {
+                                await toggleRecord()
+                            } else {
+                                isUnauthorized = true
+                            }
+                        }
+                    }) {
+                        Image(systemName: screenRecorder.isRunning ? "stop.circle" : "play.circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    Slider(value: $opacity, in: 0.3...1.0)
+                        .frame(width: 100)
+                        .onChange(of: opacity) {
+                            window?.alphaValue = opacity
+                        }
+                    
+                    Button(action: {
+                        screenRecorder.isAppExcluded.toggle()
+                    }) {
+                        Image(systemName: screenRecorder.isAppExcluded ? "eye.slash" : "eye")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    Button(action: {
+                        isWindowFloating.toggle()
+                        if let window = window {
+                            window.level = isWindowFloating ? .floating : .normal
+                        }
+                    }) {
+                        Image(systemName: isWindowFloating ? "rectangle.on.rectangle" : "rectangle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    Button(action: createNewWindow) {
+                        Image(systemName: "plus.square.on.square")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .buttonStyle(PlainButtonStyle())
                 }
-                .buttonStyle(PlainButtonStyle())
-                                
-                Button(action: createNewWindow) {
-                    Image(systemName: "plus.square.on.square")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
+            
+            if (!screenRecorder.isRunning) {
+                Color.black.opacity(0.5) // Dim background
+                    .ignoresSafeArea()
+                VStack {
+                    Button(action: {
+                        Task {
+                            await toggleRecord()
+                        }
+                    }) {
+                        Image(systemName: "play.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.white)
+                            .background(Color.clear)
+                    }.buttonStyle(PlainButtonStyle())
                 }
-                .buttonStyle(PlainButtonStyle())
             }
+        }
     }
-    
     
     private func createNewWindow() {
         if let window = window {
@@ -151,6 +166,16 @@ struct ContentView: View {
             newWindow.makeKeyAndOrderFront(self)
         }
     }
+    
+    private func toggleRecord() async {
+        if (screenRecorder.isRunning) {
+            await screenRecorder.stop()
+        }
+        else {
+            await screenRecorder.start()
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
